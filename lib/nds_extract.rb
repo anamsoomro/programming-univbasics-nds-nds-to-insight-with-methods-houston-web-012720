a246d47c57b60abcc0b8d3981db0ddfbce1774eb
anam soomro <anamsoomroed@gmail.com>
2020-01-01 20:18:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,7 @@ def gross_for_director(director_data)
   column = 0
   total = 0
   while column < director_data[:movies].length do
-    total += director_data[:movies][column][worldwide_gross]
+    total += director_data[:movies][column][:worldwide_gross]
     column += 1
   end
 end
