33053458900c4a89ff9787b8ced6ec14f29e5036
anam soomro <anamsoomroed@gmail.com>
2020-01-01 20:19:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ def gross_for_director(director_data)
     total += director_data[:movies][column][:worldwide_gross]
     column += 1
   end
-  t0ta
+  total
 end
 
 # Write a method that, given an NDS creates a new Hash
